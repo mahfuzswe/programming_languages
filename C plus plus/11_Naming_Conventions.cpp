5c8efedef2a7ef6e071269c8d12eea6e79d31437
Invincible-21 <shantobd1969@gmail.com>
2022-11-12 06:56:00 +0600

    work hard

---
+++ C plus plus/11_Naming_Conventions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //file_size --> snake case
+
+    return 0;
+}
+
--- C plus plus/tester.cpp
+++ C plus plus/tester.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello World!";
+    const double pi = 3.14;
+    cout<<pi<<endl;
     return 0;
 }
 
